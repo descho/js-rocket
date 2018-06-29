748cccb0cad5ba4a408f0aba5b131d7d1764019e
Des Cho <d.chongarova@gmail.com>
2018-06-29 22:23:20 +0100

    Made the rocket cool with drawings etc.

--- js/rocket.js
+++ js/rocket.js
@@ -12,6 +12,21 @@ var changeState = function (state) {
 		timer = setInterval(function () {
 			countdownNumber = countdownNumber-1;
 			document.getElementById('countdown').innerHTML = countdownNumber;
+			
+			if (countdownNumber > 4 && countdownNumber <= 7) {
+				// be nervous
+				document.getElementById('nervous').className = 'nervous show';
+			} else {
+				document.getElementById('nervous').className = 'nervous';
+			}
+
+			if (countdownNumber > 1 && countdownNumber <= 4) {
+				// can't wait
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			} else {
+				document.getElementById('cant-wait').className = 'cant-wait';
+			}
+
 			if (countdownNumber <= 0) {
 				changeState(3);
 			};
@@ -22,7 +37,7 @@ var changeState = function (state) {
 				var randomNumber = Math.round(Math.random()*10);
 				console.log('randomNumber:', randomNumber)
 				// success
-				if (randomNumber > 5) {
+				if (randomNumber > 2) {
 					changeState(4);
 				} else {
 					changeState(5); // oh no!
